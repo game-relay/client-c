b7f9ddb8427a2f5f264f350de3c6bd03e2a9842a
Peter Bjorklund <piot@hotmail.com>
2024-04-04 13:20:15 +0200

    chore: move from piot

---
+++ src/include/relay-client/debug.h
@@ -0,0 +1,12 @@
+/*----------------------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/game-relay/client-c
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------------------*/
+#ifndef RELAY_CLIENT_DEBUG_H
+#define RELAY_CLIENT_DEBUG_H
+
+struct RelayListener;
+
+void relayListenerDebugOutput(const struct RelayListener* self);
+
+#endif
---
+++ src/lib/debug.c
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/game-relay/client-c
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------------------*/
+#include <relay-client/debug.h>
+#include <relay-client/listener.h>
+
+#if defined CLOG_LOG_ENABLED
+
+static const char* stateToString(RelayListenerState state)
+{
+    switch (state) {
+        case RelayListenerStateIdle:
+            return "idle";
+        case RelayListenerStateConnecting:
+            return "connecting";
+        case RelayListenerStateConnected:
+            return "connected";
+    }
+
+    return "unknown";
+}
+
+#endif
+
+void relayListenerDebugOutput(const RelayListener* self)
+{
+#if defined CLOG_LOG_ENABLED
+    CLOG_C_INFO(&self->log, "state: %s", stateToString(self->state))
+#else
+    (void) self;
+#endif
+}
